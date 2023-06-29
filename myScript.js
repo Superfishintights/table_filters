6355cdc6a386e1b2ee8771917d0bb735278b529d
Superfishintights <jay.millington87@gmail.com>
2023-06-29 20:59:36 +0100

    update

--- myScript.js
+++ myScript.js
@@ -49,23 +49,182 @@ listData  = [{"first_name":"Dun","last_name":"McIlharga","make":"Acura","model":
 {"first_name":"Siegfried","last_name":"Vice","make":"BMW","model":"M5","gender":"Male"},
 {"first_name":"Skelly","last_name":"Dunthorne","make":"Volvo","model":"C70","gender":"Male"}]
 
-// Function to populate the table
-function populateTable(data) {
-    var table = document.getElementById('myTable');
-    data.forEach(function(item) {
-        var row = table.insertRow(-1); // Insert a new row at the end of the table
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        cell1.innerHTML = item['first_name'];
-        cell2.innerHTML = item['last_name'];
-        cell3.innerHTML = item['make'];
-        cell4.innerHTML = item['model'];
-        cell5.innerHTML = item['gender'];
-    });
-}
+// // Function to populate the table
+// function populateTable(data) {
+//     var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+//     data.forEach(function(item) {
+//         var row = table.insertRow(-1); // Insert a new row at the end of the table
+//         var cell1 = row.insertCell(0);
+//         var cell2 = row.insertCell(1);
+//         var cell3 = row.insertCell(2);
+//         var cell4 = row.insertCell(3);
+//         var cell5 = row.insertCell(4);
+//         cell1.innerHTML = item['first_name'];
+//         cell2.innerHTML = item['last_name'];
+//         cell3.innerHTML = item['make'];
+//         cell4.innerHTML = item['model'];
+//         cell5.innerHTML = item['gender'];
+//     });
 
-// Populate the table with the data
-populateTable(listData);
+//     // Initialize DataTable
+//     $('#myTable').DataTable();
+// }
+
+// // Populate the table with the data
+// populateTable(listData);
+
+
+// // Function to sort table by column
+// function sortTable(n) {
+// 	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+// 	table = document.getElementById("myTable");
+// 	switching = true;
+// 	dir = "asc";
+
+// 	while (switching) {
+// 		switching = false;
+// 		rows = table.rows;
+
+// 		for (i = 1; i < rows.length - 1; i++) {
+// 			shouldSwitch = false;
+// 			x = rows[i].getElementsByTagName("TD")[n];
+// 			y = rows[i + 1].getElementsByTagName("TD")[n];
+
+// 			if (dir == "asc") {
+// 				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+// 					shouldSwitch = true;
+// 					break;
+// 				}
+// 			} else if (dir == "desc") {
+// 				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+// 					shouldSwitch = true;
+// 					break;
+// 				}
+// 			}
+// 		}
+
+// 		if (shouldSwitch) {
+// 			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+// 			switching = true;
+// 			switchcount++;
+// 		} else {
+// 			if (switchcount == 0 && dir == "asc") {
+// 				dir = "desc";
+// 				switching = true;
+// 			}
+// 		}
+// 	}
+// }
+
+// // Add event listeners to table headers
+// window.onload = function() {
+// 	document.getElementById("headerFN").addEventListener("click", function(){sortTable(0)});
+// 	document.getElementById("headerLN").addEventListener("click", function(){sortTable(1)});
+// 	document.getElementById("headerMake").addEventListener("click", function(){sortTable(2)});
+// 	document.getElementById("headerModel").addEventListener("click", function(){sortTable(3)});
+// 	document.getElementById("headerGender").addEventListener("click", function(){sortTable(4)});
+// 	populateTable(listData);
+// }
+
+// $(document).ready(function() {
+//     var table = $('#myTable').DataTable();
+
+//     var genderSelect = new SlimSelect({
+//         select: '#genderSelect',
+//         onChange: function(selectedValues) {
+//             table
+//                 .column(4) // Gender is the 5th column (0-indexed)
+//                 .search(selectedValues.join('|'), true, false) // Use regex to match any of the selected values
+//                 .draw();
+//         }
+//     });
+
+//     var makeSelect = new SlimSelect({
+//         select: '#makeSelect',
+//         onChange: function(selectedValues) {
+//             table
+//                 .column(2) // Make is the 3rd column
+//                 .search(selectedValues.join('|'), true, false) // Use regex to match any of the selected values
+//                 .draw();
+//         }
+//     });
+
+//     // Populate the table with the data
+//     populateTable(listData);
+
+//     // Add options to the Slim Select dropdowns based on the unique values in each column
+//     table.column(4).data().unique().sort().each(function(value) {
+//         genderSelect.addOption({text: value, value: value});
+//     });
+
+//     table.column(2).data().unique().sort().each(function(value) {
+//         makeSelect.addOption({text: value, value: value});
+//     });
+
+//     // Show the corresponding dropdown when the header is clicked
+//     $('#myTable th').click(function() {
+//         var index = $(this).index();
+//         if (index === 4) { // Gender is the 5th column
+//             $('#genderSelect').show();
+//             genderSelect.open();
+//         } else if (index === 2) { // Make is the 3rd column
+//             $('#makeSelect').show();
+//             makeSelect.open();
+//         }
+//     });
+// });
+
+$(document).ready(function() {
+    var table = $('#myTable').DataTable({
+        data: listData,
+        columns: [
+            { data: 'first_name', orderable: false },
+            { data: 'last_name', orderable: false },
+            { data: 'make', orderable: false },
+            { data: 'model', orderable: false },
+            { data: 'gender', orderable: false }
+        ]
+    });
+
+    // Initialize the SlimSelect dropdowns and append them to the corresponding headers
+    var genderSelect = new SlimSelect({
+        select: '#genderSelect',
+        onChange: function(selectedValues) {
+            table
+                .column(4)
+                .search(selectedValues.join('|'), true, false)
+                .draw();
+        }
+    });
+    $('#genderSelect').hide().appendTo('#headerGender');
+
+    var makeSelect = new SlimSelect({
+        select: '#makeSelect',
+        onChange: function(selectedValues) {
+            table
+                .column(2)
+                .search(selectedValues.join('|'), true, false)
+                .draw();
+        }
+    });
+    $('#makeSelect').hide().appendTo('#headerMake');
+
+    // Show the corresponding dropdown and populate it with unique values when the header is clicked
+    $('#headerGender').click(function() {
+        $('#genderSelect').show();
+        genderSelect.setData([]);
+        table.column(4).data().unique().sort().each(function(value) {
+            genderSelect.addOption({text: value, value: value});
+        });
+        genderSelect.open();
+    });
+
+    $('#headerMake').click(function() {
+        $('#makeSelect').show();
+        makeSelect.setData([]);
+        table.column(2).data().unique().sort().each(function(value) {
+            makeSelect.addOption({text: value, value: value});
+        });
+        makeSelect.open();
+    });
+});
